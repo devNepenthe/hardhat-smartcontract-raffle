5975d408a263123f50322fa80b2509876da7543e
devNepenthe <ljst.web3@proton.me>
2023-10-26 16:59:45 +0800

    add dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,13 @@
+require("@nomicfoundation/hardhat-ethers");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-verify");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("solidity-coverage");
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
