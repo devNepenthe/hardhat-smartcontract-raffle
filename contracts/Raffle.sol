713115fbd5f5dd5909eab706560f9a0277051822
devNepenthe <ljst.web3@proton.me>
2023-10-27 02:19:27 +0800

    inherit chainlink VRF

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -8,12 +8,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
+
 error Raffle__NotEnoughETH();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* Type Declarations */
 
     /* State Variables */
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinatorV2;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
@@ -23,8 +34,18 @@ contract Raffle {
     /* Modifiers */
 
     /* Functions */
-    constructor(uint256 entranceFee) {
+    constructor(
+        uint256 entranceFee,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinatorV2 = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -36,6 +57,12 @@ contract Raffle {
     }
 
     // function pickRandomWinner() public {}
+    function requestRandomWords() external {}
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
