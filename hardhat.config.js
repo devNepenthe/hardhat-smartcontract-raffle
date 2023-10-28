8514eac5295c61918c2dbb408ef27a9c3d6ef5f4
devNepenthe <ljst.web3@proton.me>
2023-10-28 21:12:11 +0800

    add networks, namedAccounts, etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,38 @@ require("hardhat-contract-sizer");
 require("solidity-coverage");
 require("dotenv").config();
 
+SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+PRIVATE_KEY = process.env.PRIVATE_KEY;
+ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            // accounts: [],
+            chainId: 31337,
+        },
+    },
     solidity: "0.8.8",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            sepolia: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
