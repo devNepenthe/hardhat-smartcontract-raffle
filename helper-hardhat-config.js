f61aaf1a1eca484bb4bc9e353cdc35c8e11df58e
devNepenthe <ljst.web3@proton.me>
2023-11-09 02:59:30 +0800

    add sepolia subscriptionId

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -15,6 +15,7 @@ const networkConfig = {
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
         callbackGasLimit: "500000",
         updateInterval: "30",
+        subscriptionId: "6756",
     },
 };
 
